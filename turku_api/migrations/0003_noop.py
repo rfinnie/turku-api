cedfa7dad700f0085976ffa8519bf41d83b0db6d
Ryan Finnie <ryan.finnie@canonical.com>
2015-02-27 23:12:29 -0800

    Fix bad models config in 0002 with noop 0003

--- turku_api/migrations/0003_noop.py
+++ turku_api/migrations/0003_noop.py
@@ -8,27 +8,11 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # https://code.djangoproject.com/ticket/22844
-        db.delete_unique(u'turku_api_source', ['machine_id', 'name'])
-
-        # Adding field 'Source.published'
-        db.add_column(u'turku_api_source', 'published',
-                      self.gf('django.db.models.fields.BooleanField')(default=True),
-                      keep_default=False)
-
-        # https://code.djangoproject.com/ticket/22844
-        db.create_unique(u'turku_api_source', ['machine_id', 'name'])
+        pass
 
 
     def backwards(self, orm):
-        # https://code.djangoproject.com/ticket/22844
-        db.delete_unique(u'turku_api_source', ['machine_id', 'name'])
-
-        # Deleting field 'Source.published'
-        db.delete_column(u'turku_api_source', 'published')
-
-        # https://code.djangoproject.com/ticket/22844
-        db.create_unique(u'turku_api_source', ['machine_id', 'name'])
+        pass
 
 
     models = {
@@ -37,7 +21,7 @@ class Migration(SchemaMigration):
             'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'comment': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
             'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'id': ('django.db.models.fields.CharField', [], {'default': "'5f274f06-d657-434c-9b2d-67ad0e479b1c'", 'max_length': '36', 'primary_key': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'default': "'a775c5cf-3eea-4571-90a2-55c68f1a5fc9'", 'max_length': '36', 'primary_key': 'True'}),
             'secret': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'}),
             'secret_type': ('django.db.models.fields.CharField', [], {'max_length': '200'})
         },
@@ -50,7 +34,7 @@ class Migration(SchemaMigration):
             'date_registered': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'date_updated': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'environment_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.CharField', [], {'default': "'279f55db-a4ff-4a00-a0bb-2fdbd908a5fd'", 'max_length': '36', 'primary_key': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'default': "'23dc7d8e-7d0d-4ffe-908a-3dcaee1bd577'", 'max_length': '36', 'primary_key': 'True'}),
             'secret': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
             'service_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
             'ssh_public_key': ('django.db.models.fields.CharField', [], {'max_length': '2048'}),
@@ -59,7 +43,7 @@ class Migration(SchemaMigration):
             'uuid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '36'})
         },
         u'turku_api.source': {
-            'Meta': {'object_name': 'Source'},
+            'Meta': {'unique_together': "(('machine', 'name'),)", 'object_name': 'Source'},
             'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'comment': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
             'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
@@ -68,13 +52,14 @@ class Migration(SchemaMigration):
             'date_updated': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'exclude': ('django.db.models.fields.CharField', [], {'default': "'[]'", 'max_length': '2048'}),
             'frequency': ('django.db.models.fields.CharField', [], {'default': "'daily'", 'max_length': '200'}),
-            'id': ('django.db.models.fields.CharField', [], {'default': "'5472d78e-f55c-43a1-af17-4eb4d17144f4'", 'max_length': '36', 'primary_key': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'default': "'dee7b3b6-c0cf-4c6d-94cf-bfc55cb2ab0c'", 'max_length': '36', 'primary_key': 'True'}),
             'large_modifying_files': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'large_rotating_files': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'machine': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['turku_api.Machine']"}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
             'path': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'retention': ('django.db.models.fields.CharField', [], {'default': "'last 5 days,earliest of month'", 'max_length': '200'}),
             'shared_service': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'username': ('django.db.models.fields.CharField', [], {'max_length': '200'})
@@ -87,7 +72,7 @@ class Migration(SchemaMigration):
             'date_checked_in': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
             'date_registered': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'date_updated': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'id': ('django.db.models.fields.CharField', [], {'default': "'3c920ee4-b744-41ca-bbb5-32aa7bea799f'", 'max_length': '36', 'primary_key': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'default': "'28d320ef-3e0f-4888-8ee9-9f9d5687d5cc'", 'max_length': '36', 'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'}),
             'secret': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
             'ssh_ping_host': ('django.db.models.fields.CharField', [], {'max_length': '200'}),

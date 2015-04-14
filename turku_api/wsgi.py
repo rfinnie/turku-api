e6def7143b6851ccfe50dce8d8cbfea409f73958
Ryan Finnie <ryan.finnie@canonical.com>
2015-04-13 22:48:21 -0700

    Allow use of wsgi.py in-place

--- turku_api/wsgi.py
+++ turku_api/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turku_api.settings")
 
 from django.core.wsgi import get_wsgi_application

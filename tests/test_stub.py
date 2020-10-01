ff9207e0feabcf3e3e355d2692409c99169b5720
Ryan Finnie <ryan@finnie.org>
2020-09-30 20:21:00 -0700

    Remove no longer needed test_stub.py

--- tests/test_stub.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import warnings
-
-
-class TestStub(unittest.TestCase):
-    def test_stub(self):
-        # pytest doesn't like a tests/ with no tests
-        warnings.warn("Remove this file once unit tests are added")

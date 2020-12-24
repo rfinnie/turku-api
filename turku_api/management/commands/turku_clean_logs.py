e2d56b98d79b31e687b68571dfbab542777f33be
Ryan Finnie <ryan@finnie.org>
2020-12-24 15:08:19 -0800

    Add turku_clean_logs management command

---
+++ turku_api/management/commands/turku_clean_logs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Turku backups
+# Copyright (C) 2015-2020 Canonical Ltd., Ryan Finnie and other contributors
+#
+# SPDX-License-Identifer: AGPL-3.0-or-later
+
+import datetime
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from turku_api.models import BackupLog
+
+
+class Command(BaseCommand):
+    help = "Turku clean logs"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--days",
+            type=int,
+            required=True,
+            help="Clean logs older than this number of days",
+        )
+
+    def handle(self, *args, **options):
+        for backuplog in BackupLog.objects.filter(
+            date_end__lt=(timezone.now() - datetime.timedelta(days=options["days"]))
+        ):
+            backuplog.delete()

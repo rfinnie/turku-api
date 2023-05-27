a99cbab0a43c4b4387b2a1e63ba979f1a8ae87ee
Ryan Finnie <ryan@finnie.org>
2023-05-27 09:32:42 -0700

    Convert setup.py to setup.cfg, bump minimum to 3.8

--- setup.py
+++ setup.py
@@ -5,24 +5,7 @@
 # SPDX-FileCopyrightText: Copyright (C) 2015-2021 Ryan Finnie <ryan@finnie.org>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-import os
-from setuptools import setup
+import setuptools
 
-
-def read(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename), encoding="utf-8") as f:
-        return f.read()
-
-
-setup(
-    name="turku_api",
-    description="Turku backups - API application",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    author="Ryan Finnie",
-    url="https://github.com/rfinnie/turku-api",
-    python_requires="~=3.4",
-    packages=["turku_api", "turku_api.management.commands"],
-    include_package_data=True,
-    install_requires=["Django", "tzdata"],
-)
+if __name__ == "__main__":
+    setuptools.setup()

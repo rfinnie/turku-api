43237c8efca082490025b73d57eca034fe53ad73
Ryan Finnie <ryan@finnie.org>
2026-05-01 10:49:28 -0700

    Bump requires-python; add explicit build-system; add stub setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# SPDX-PackageName: turku-api
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://codeberg.org/rfinnie/turku-api
+# SPDX-FileCopyrightText: © 2026 Ryan Finnie <ryan@finnie.org>
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from setuptools import setup
+
+setup()
